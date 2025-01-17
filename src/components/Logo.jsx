aaf9681066a64f4842f2425f3e589314a29b8a88
jitin patel <jitin.21233@knit.ac.in>
2025-01-18 01:01:17 +0530

    Initial commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Logo({width = '100px'}) {
+  return (
+    <div className=' w-12 mx-0'>
+      <img src="https://cdn.logojoy.com/wp-content/uploads/2018/05/30164225/572-768x591.png" alt="Descriptive text for the image"></img>
+    </div>
+  )
+}
+
+export default Logo
